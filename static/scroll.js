800ce7cb50a007af222a6e6928c00d9c00890705
Kusumayadav19 <kushhhyadav19@gmail.com>
2024-11-02 09:48:09 +0530

    add all

---
+++ static/scroll.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const greenLine = document.getElementById('greenLine');
+
+    function animateLine() {
+        greenLine.style.width = '100%';
+    }
+
+    // Start the animation
+    animateLine();
+});
